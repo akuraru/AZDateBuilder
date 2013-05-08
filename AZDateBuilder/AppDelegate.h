0d28441f43925af2c3b9796d083b9fb352ba9a29
azu <azuciao@gmail.com>
2013-05-09 00:30:44 +0900

    Initial Commit

---
+++ AZDateBuilder/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AZDateBuilder
+//
+//  Created by azu on 2013/05/09.
+//  Copyright (c) 2013年 azu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
